ebfd4f5f14112177626bc8a36b4d3bab5c739eb6
Raj Ghamsani <54090909+rajpateln1995@users.noreply.github.com>
2020-02-03 23:25:27 +0530

    Final

---
+++ src/login/string.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package login;
+
+/**
+ *
+ * @author Raj Ghamsani
+ */
+class string extends list{
+    
+    
+    String custName;
+    String date;
+    int quantity;
+    String medName=c1.name;
+    int price=c1.sell;
+}
